53b5be4a7715f9663024e285a859bea30fb07431
Manuel Rigonat <manuelrigonat@hotmail.com>
2017-11-03 16:56:00 -0300

    ver

--- GestorDeTareas/src/model/dao/ProyectoDAO.java
+++ GestorDeTareas/src/model/dao/ProyectoDAO.java
@@ -6,17 +6,10 @@ import model.*;
 
 public interface ProyectoDAO {
 
-	public boolean agregarProyecto(long id, String nombre, Date fecha_inicio, Date fecha_estimada);
+	public boolean crearProyecto(long id, String nombre, Date fecha_inicio, Date fecha_estimada);
 	public boolean eliminarProyecto(long id);
 	public boolean editarProyecto(long id, String nombre, Date fecha_inicio, Date fecha_estimada);
-	
-	public boolean agregarMiembro(Usuario user);
-	public boolean eliminarMiembro(Usuario u);
-	
-	public boolean agregarColumna(Columna c);
-	public boolean eliminarColumna(Columna c);
-	public ArrayList<Columna> getColumnas();
-	
+	public boolean recuperarProyecto(Proyecto proyecto);
 	
 	
 	
