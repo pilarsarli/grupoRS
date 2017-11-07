75094fbfb7cb247c0e03a2c4501b9a51f2afc7ae
Manuel Rigonat <manuelrigonat@hotmail.com>
2017-11-07 15:18:59 -0300

    update

--- GestorDeTareas/src/model/dao/GenericDAO.java
+++ GestorDeTareas/src/model/dao/GenericDAO.java
@@ -1,10 +1,12 @@
 package model.dao;
 
+import java.io.Serializable;
+
 public interface GenericDAO<T> {
 	
-	public boolean actualizar(T entity);
-	public boolean eliminar(T entity);
-	public boolean guardar(T entity);
+	public T actualizar(T entity);
+	public void eliminar(T entity);
+	public T eliminar(Serializable id);
+	public T guardar(T entity);
 	public T recuperar(long id);
-	public boolean existe(T entity);
 }
---
+++ GestorDeTareas/src/model/daoHibernateJPA/GenericDaoHibernateJPA.java
@@ -0,0 +1,90 @@
+package model.daoHibernateJPA;
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import model.dao.GenericDAO;
+
+public class GenericDaoHibernateJPA<T> implements GenericDAO<T> {
+	private String Service = null;
+	
+	protected EntityManager getEMF() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(Service);
+		EntityManager em = emf.createEntityManager();
+		return em;
+	}
+	public T actualizar(T entity) {
+		EntityManager em = getEMF();
+		EntityTransaction etx= em.getTransaction();
+		etx.begin();
+		T ent = em.merge(entity);
+		etx.commit();
+		em.close();
+		return ent;
+	}
+
+	@Override
+	public void eliminar(T entity) {
+		EntityManager em = getEMF();
+		EntityTransaction tx = null;
+		try {
+			tx = em.getTransaction();
+			tx.begin();
+			em.remove(entity);
+			tx.commit();
+		}
+		catch (RuntimeException e) {
+		if ( tx != null && tx.isActive() ) tx.rollback();
+		throw e; // escribir en un log o mostrar un mensaje
+		}
+		finally {
+		em.close();
+		}
+	}
+
+	@Override
+	public T guardar(T entity) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Gestor");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = null;
+		try {
+			tx = em.getTransaction();
+			tx.begin();
+			em.persist(entity);
+			tx.commit();
+		}
+		catch (RuntimeException e) {
+			if ( tx != null && tx.isActive() ) tx.rollback();
+			throw e; // escribir en un log o mostrar un mensaje
+		}
+		finally {
+			em.close();
+		}
+		return entity;
+		}	
+		
+	
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T recuperar(long id) {
+		EntityManager em = getEMF();
+		T entity = em.find((Class<T>) this.getPersistentClass(), id);
+		return entity;
+	}
+
+
+	public T eliminar(Serializable id) {
+		EntityManager em = getEMF();
+		T entity = em.find((Class<T>) this.getPersistentClass(), id);
+		if (entity != null) {
+			this.eliminar(entity);
+			}
+			return entity;
+	}
+	protected T getPersistentClass() {
+		return null;
+	}
+		
+
+}
