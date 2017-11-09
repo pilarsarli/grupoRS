613d6623a4498597777abed66e4ceb73a556755f
MARÍA PILAR SARLI <pilarsarli@gmail.com>
2017-11-09 15:02:49 -0300

    testing

--- GestorDeTareas/src/model/daoHibernateJPA/JPATest.java
+++ GestorDeTareas/src/model/daoHibernateJPA/JPATest.java
@@ -1,9 +1,18 @@
 package model.daoHibernateJPA;
-import model.Usuario;
+import model.*;
+import model.dao.*;
 
 public class JPATest {
 
 	public static void main(String[] args) {
+		DAOFactory f = new DAOFactory();
+		
+		UsuarioDAO user = f.getUsuarioDAO();
+		// Se crean 2 usuarios de ejemplo
+		Usuario user1 = new Usuario("usuario1","clave","nombre1","apellido1","mail1@ejemplo.com");
+		Usuario user2 = new Usuario("usuario2","clave","nombre2","apellido2","mail2@ejemplo.com");
+		user1 =  user.guardar(user1);
+		user2 =  user.guardar(user2);
 		
 	}
 
--- GestorDeTareas/src/model/daoHibernateJPA/UsuarioDaoJPA.java
+++ GestorDeTareas/src/model/daoHibernateJPA/UsuarioDaoJPA.java
@@ -1,8 +1,29 @@
 package model.daoHibernateJPA;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import model.Usuario;
 import model.dao.UsuarioDAO;
 
 public class UsuarioDaoJPA extends GenericDaoHibernateJPA<Usuario> implements UsuarioDAO {
+
+	@Override
+	public Usuario autentificacion(String usuario, String clave) {
+		EntityManager em = EMF.getEMF().createEntityManager();
+		Query q = em.createQuery("SELECT p FROM USUARIO p WHERE p.nombreUsuario = :usr AND p.clave = :clave");
+		q.setParameter("usr", usuario);
+		q.setParameter("clave", clave);
+		List<Usuario> resultado = (List<Usuario>) q.getResultList();
+		if(resultado.size()!=0){
+			long id= resultado.get(0).getIdUsuario();
+			return (this.recuperar(id)); //recupera el usuario y lo retorna
+		}
+		else{
+			return null;
+		}
+	}
 
 }
