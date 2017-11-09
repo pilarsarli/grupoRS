ef624e9212b4022046277cdb6af164c04cd3b27b
Manuel Rigonat <manuelrigonat@hotmail.com>
2017-11-09 15:03:32 -0300

    aoisdj

--- GestorDeTareas/src/model/daoHibernateJPA/UsuarioDaoJPA.java
+++ GestorDeTareas/src/model/daoHibernateJPA/UsuarioDaoJPA.java
@@ -5,4 +5,10 @@ import model.dao.UsuarioDAO;
 
 public class UsuarioDaoJPA extends GenericDaoHibernateJPA<Usuario> implements UsuarioDAO {
 
+	@Override
+	public Usuario autentificacion(String usuario, String clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
