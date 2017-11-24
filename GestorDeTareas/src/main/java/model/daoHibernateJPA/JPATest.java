5ff4f3330fdc1aeb5ab4a44239c8786173914d1a
MARÍA PILAR SARLI <pilarsarli@gmail.com>
2017-11-24 16:05:35 -0300

    CRUD con spring ok

--- GestorDeTareas/src/main/java/model/daoHibernateJPA/JPATest.java
+++ GestorDeTareas/src/main/java/model/daoHibernateJPA/JPATest.java
@@ -14,8 +14,8 @@ public class JPATest {
 		// Se crean 2 usuarios de ejemplo
 		Usuario user1 = new Usuario("usuario1","clave","nombre1","apellido1","mail1@ejemplo.com");
 		Usuario user2 = new Usuario("usuario2","clave","nombre2","apellido2","mail2@ejemplo.com");
-		user1 =  usuarioDAO.guardar(user1);
-		user2 = usuarioDAO.guardar(user2);
+		user1 = usuarioDAO.persistir(user1);
+		user2 = usuarioDAO.persistir(user2);
 		
 		// Probamos recuperarlo
 		Usuario usuarioR = usuarioDAO.recuperar(user1.getIdUsuario());
