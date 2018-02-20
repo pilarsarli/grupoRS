436c167a57ddf380c37a4eb166734dbb9625609e
MARÍA PILAR SARLI <pilarsarli@gmail.com>
2018-02-20 16:32:56 -0300

    vistas + modelando serv

--- Angular2/src/app/app.user.service.ts
+++ Angular2/src/app/app.user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 //import {of} from 'rxjs/observable/of';
-import {HttpClientModule, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
 
 import {Usuario} from './clases';
 import {Proyecto} from './clases';
@@ -18,12 +18,20 @@ const USUARIOS : Usuario[] = [
 
 @Injectable()
 export class UserService{
-
-    constructor(private http: HttpClientModule){}
+    private url = '';
+    private newUserUrl ="";
+    constructor(private http: HttpClient){}
     
     getUsuarios(): Usuario[]{ return  USUARIOS } 
     
     getProyectos(id : number): Proyecto[]{return PROYECTOS}
     
     //getProyectos(id:number): Observable<Proyecto[]>{}
+    
+    crearUsuario(user : Usuario):Observable<any>{
+        let json = JSON.stringify(user);
+        let params = "json="+json;
+        let headers = new HttpHeaders().set('Context-Type', this.newUserUrl);
+        return this.http.post(this.url+'usuarios', params, {headers:headers});
+    }
 }
